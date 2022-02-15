bd591999d72f7d9e86d694eeb5a0b21a1c3d6261
Chris Marrin <chris@marrin.com>
2022-02-14 16:26:22 -0800

    Made all the mem pointers into int16_t for error checking

--- Interpreter.h
+++ Interpreter.h
@@ -160,15 +160,15 @@ private:
         
         const uint32_t& top(uint8_t rel = 0) const { ensureRel(rel); return _stack[_sp - rel - 1]; }
         uint32_t& top(uint8_t rel = 0) { ensureRel(rel); return _stack[_sp - rel - 1]; }
-        const uint32_t& local(uint16_t addr) const { return get(addr + _bp); }
-        uint32_t& local(uint16_t addr) { return get(addr + _bp); }
+        const uint32_t& local(uint16_t addr) const { ensureLocal(addr); return get(addr + _bp); }
+        uint32_t& local(uint16_t addr) { ensureLocal(addr); return get(addr + _bp); }
 
         bool empty() const { return _sp == 0; }
         Error error() const { return _error; }
         
         bool setFrame(uint8_t params, uint8_t locals)
         {
-            uint16_t savedPC = pop();
+            int16_t savedPC = pop();
             _sp += locals;
             push(savedPC);
             push(_bp);
@@ -181,10 +181,10 @@ private:
             return true;
         }
 
-        uint16_t restoreFrame(uint32_t returnValue)
+        int16_t restoreFrame(uint32_t returnValue)
         {
-            uint16_t savedBP = pop();
-            uint16_t pc = pop();
+            int16_t savedBP = pop();
+            int16_t pc = pop();
             _sp = _bp;
             _bp = savedBP;
             push(returnValue);
@@ -209,6 +209,15 @@ private:
             }
         }
         
+        void ensureLocal(uint8_t i) const
+        {
+            int16_t addr = _bp + i;
+            if (addr < 0 || addr >= _sp) {
+                // Set the error, but let the exec catch it
+                _error = Error::StackOutOfRange;
+            }
+        }
+        
         void ensurePush() const
         {
             if (_sp >= _size) {
@@ -219,7 +228,7 @@ private:
         
         uint32_t& get(uint16_t addr) const
         {
-            if (addr >= _size) {
+            if (int16_t(addr) >= _size) {
                 // Set the error, but let the exec catch it
                 _error = Error::StackOverrun;
             }
@@ -227,9 +236,9 @@ private:
         }
         
         uint32_t* _stack = nullptr;
-        uint16_t _size = 0;
-        uint16_t _sp = 0;
-        uint16_t _bp = 0;
+        int16_t _size = 0;
+        int16_t _sp = 0;
+        int16_t _bp = 0;
         mutable Error _error = Error::None;
     };
 
@@ -351,7 +360,7 @@ private:
     uint32_t* _global = nullptr;
     uint16_t _globalSize = 0;
     
-    uint16_t _pc = 0;
+    int16_t _pc = 0;
     Stack _stack;
     
     uint16_t _constOffset = 0; // In bytes
