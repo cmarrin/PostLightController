08187995dc6aaf03b5ac4506376661f2db23dbb4
Chris Marrin <chris@marrin.com>
2026-05-21 12:30:04 -0700

    Improved f.lua animation
    - Now animates a float from 0 to 1, then scales it by the passed in brightness to give the final value
    - Derive the range of animation increments from speed
    - Allow incoming brightness values from 0 to 255, which are scaled to be between the minimum brightness allowed for the NeoPixels (to keep them from strobing) and 255.
    - Guarantees that max brightness is enough greater than the minimum to get a good flicker effect

--- f.lua
+++ f.lua
@@ -11,32 +11,37 @@
 --          3           Speed (0-7)
 --
 -- Flicker inc value. This is a random value based on speed.
--- Speed goes from 0-7 for flicker. Min and max values are
--- from 0 to 255 multiplied by 128. so a typical range is
--- 15000. If we want to animate 15000 values in 1 second
--- at 25ms per cycle, the inc value would be 375. A slow
--- animation would be around 5 seconds (inc = 2000). A fast
--- animation would be around 1/2 second (inc = 200).
--- These table entries are /128 (so they fit in a byte)
--- so they should go from a slow of 1 to a fast of 16
-
--- Animation values are 16 bit integers, representing a fixed point
--- number, with 9 bits representing the signed integer part and 7 
--- bits for the fraction. So numbers can be +/- 255 with 2 digits 
--- of decimal precision. To get a brightness value from 'cur' you 
--- simply shift right 7 places or divide by 128.
-
--- Flicker need all LedEntries because all leds on all posts flicker independently.
+-- Speed goes from 0-7 for flicker. We use floating point
+-- values from 0 to 1. It's 30ms per animation step. A slow 
+-- flicker is over 5 seconds so for speed 0 the inc should be
+-- 0.006. A fast flicker is 8 times faster, so 0.625 seconds, 
+-- so for speed 7 the inc should be 0.05. We add some randomness 
+-- of around 10%, so the inc values will be determined by:
+--
+--		incMin = 1 / ((8 - speed) * 625 / 30)
+--		incMax = incMin * 1.1
+--
+-- The current brightness value (ledCur) is the current animated
+-- value, from 0 to 1. We scale that from FlickerBrightnessMin,
+-- a value chosen to prevent the LEDs from strobing, to the
+-- passed in 'v' value.
+-- 
+-- Flicker needs all LedEntries because all leds on all posts flicker independently.
 -- So the first PixelsPerPost entries are for the first post, the next entries
 -- are for the second post and so on.
+
+local TestTiming = false
 
 local PixelsPerPost = 8
 local NumPosts = 7
 local NumPixels = PixelsPerPost * NumPosts
-local Delay = 30; -- Delay between iterations (in ms)
+local Delay = 30 -- Delay between iterations (in ms)
 
-local FlickerMin = 38
-local FlickerBrightestMin = 77;
+local FlickerBrightnessMin = 77
+local FlickerBrightnessMaxMin = 120 -- brightnessMax has to be this much more than FlickerBrightnessMin so we get flicker
+
+local IncMinMaxDiff = 1.2
+local IncConstant = 400 -- value multiplied by speed and then inverted to get base increment (lower numbers are faster)
 
 function clearArray(array, size)
 	for i = 1, size, 1 do
@@ -58,17 +63,17 @@ if speed > 7 then
 	speed = 7
 end
 
-local brightnessMax = math.max(FlickerBrightestMin, math.min(255, v))
+-- v gets scaled  so v = 0 to 255 and maxBrightness = FlickerBrightnessMaxMin to 255
+local brightnessMax = math.max(0, math.min(255, v))
+brightnessMax = brightnessMax / 255 * (255 - FlickerBrightnessMaxMin) + FlickerBrightnessMaxMin
 
-local speedMin = speed + 1
-local speedMax = speed + 2
+local incMin = 1 / ((8 - speed) * IncConstant / Delay)
+local incMax = incMin * IncMinMaxDiff
 
 local ledCur = { }
 clearArray(ledCur, NumPixels)
 local ledInc = { }
 clearArray(ledInc, NumPixels)
-local ledMin = { }
-clearArray(ledMin, NumPixels)
 local ledMax = { }
 clearArray(ledMax, NumPixels)
 
@@ -80,9 +85,9 @@ function animate(index)
             ledCur[index] = ledMax[index]
             return 1
         end
-    elseif ledCur[index] <= ledMin[index] - ledInc[index] then
+    elseif ledCur[index] <= ledInc[index] then
 		ledInc[index] = -ledInc[index]
-		ledCur[index] = ledMin[index]
+		ledCur[index] = 0
 		return -1
 	end
 
@@ -90,24 +95,29 @@ function animate(index)
     return 0
 end
 
+if TestTiming then
+	t = millis()
+	timeCount = 100
+end
+
 while true do
 	local loopStartTime = millis();
 	for i = 1, NumPixels, 1 do
 		if animate(i) == -1 then
-			-- We are done with the throb. We always start at BrightnessMin.
+			-- We are done with the throb. We always start at 0
+			ledCur[i] = 0
+			
 			-- Select a new inc (how fast it pulses), and  max (how bright it
-			-- gets) based on the speed value.
-			ledCur[i] = FlickerMin * 128
-			ledMin[i] = ledCur[i]
+			-- gets) by setting the inc to a random value between incMin and incMax
+			ledInc[i] = math.random() * (incMax - incMin) + incMin
 			
-			-- Set the inc to a random value from the table
-			ledInc[i] = math.random(speedMin, speedMax) * 128
-			
-			-- set the max brightness for flicker
-			ledMax[i] = math.random(FlickerBrightestMin, brightnessMax) * 128
+			-- set the max brightness to a random value
+			ledMax[i] = math.random()
 		end
-
-		setLED(1, i - 1, hsvToRGB(h, s, ledCur[i] / 128))
+		
+		-- ledCur is a value between 0 (FlickerBrightestMin) and 1 (brightnessMax)
+		local brightness = ledCur[i] * (brightnessMax - FlickerBrightnessMin) + FlickerBrightnessMin
+		setLED(1, i - 1, hsvToRGB(h, s, brightness))
 	end
 	refreshLEDs(1)
 	
@@ -116,4 +126,15 @@ while true do
 		delayTime = 1
 	end
 	delay(delayTime)
+
+	if TestTiming then
+		timeCount = timeCount - 1
+		if timeCount == 0 then
+			local t2 = millis()
+			local diff = t2 - t
+			t = t2
+			timeCount = 100
+			__print__("difftime is "..(diff / 100).."\n")
+		end
+	end
 end
