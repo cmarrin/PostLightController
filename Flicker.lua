2fe208c138519cf37629d9f1ba32b9f7a3fd1085
Chris Marrin <chris@marrin.com>
2026-06-05 17:06:24 -0700

    Added a Flicker Lua program intended to be run inside uipanel.lua

---
+++ Flicker.lua
@@ -0,0 +1,130 @@
+-- Lua script for Flicker Post Light Controller effect
+--
+-- Args are sent in as an array to the setup() function:
+--
+--		0, 1, 2		- Color (hsv) 0 to 255. Clamped by hsvToRGB() if out of range
+--		3			- Speed 0 (slow) to 7 (fast). Values out of range are clamped.
+--
+-- Flicker inc value. This is a random value based on speed.
+-- Speed goes from 0-7 for flicker. We use floating point
+-- values from 0 to 1. It's 30ms per animation step. A slow 
+-- flicker is over 5 seconds so for speed 0 the inc should be
+-- 0.006. A fast flicker is 8 times faster, so 0.625 seconds, 
+-- so for speed 7 the inc should be 0.05. We add some randomness 
+-- of around 10%, so the inc values will be determined by:
+--
+--		incMin = 1 / ((8 - speed) * 625 / 30)
+--		incMax = incMin * 1.1
+--
+-- The current brightness value (ledCur) is the current animated
+-- value, from 0 to 1. We scale that from FlickerBrightnessMin,
+-- a value chosen to prevent the LEDs from strobing, to the
+-- passed in 'v' value.
+-- 
+-- Flicker needs all LedEntries because all leds on all posts flicker independently.
+-- So the first PixelsPerPost entries are for the first post, the next entries
+-- are for the second post and so on.
+
+local Effect = { }
+
+Effect.Delay = 30 -- Delay between iterations (in ms)
+
+Effect.FlickerBrightnessMin = 77
+Effect.FlickerBrightnessMaxMin = 120 -- brightnessMax has to be this much more than FlickerBrightnessMin so we get flicker
+
+Effect.IncMinMaxDiff = 1.2
+Effect.IncConstant = 400 -- value multiplied by speed and then inverted to get base increment (lower numbers are faster)
+
+Effect.hue = 0
+Effect.sat = 0
+Effect.val = 0
+Effect.speed = 0
+
+Effect.brightnessMax = 0
+Effect.incMin = 0
+Effect.incMax = 0
+
+function Effect.clearArray(array, size)
+	for i = 1, size, 1 do
+		array[i] = 0;
+	end
+end
+
+Effect.ledCur = { }
+Effect.clearArray(Effect.ledCur, NumPixels)
+Effect.ledInc = { }
+Effect.clearArray(Effect.ledInc, NumPixels)
+Effect.ledMax = { }
+Effect.clearArray(Effect.ledMax, NumPixels)
+
+function Effect.setup(arg)
+	-- Get the params
+	Effect.hue = tonumber(arg[1])
+	Effect.sat = tonumber(arg[2])
+	Effect.val = tonumber(arg[3])
+	Effect.speed = tonumber(arg[4])
+
+	if Effect.speed < 0 then
+		Effect.speed = 0
+	end
+
+	if Effect.speed > 7 then
+		Effect.speed = 7
+	end
+
+	-- val gets scaled  so it is 0 to 255 and maxBrightness = FlickerBrightnessMaxMin to 255
+	Effect.brightnessMax = math.max(0, math.min(255, Effect.val))
+	Effect.brightnessMax = Effect.brightnessMax / 255 * (255 - Effect.FlickerBrightnessMaxMin) + Effect.FlickerBrightnessMaxMin
+
+	Effect.incMin = 1 / ((8 - Effect.speed) * Effect.IncConstant / Effect.Delay)
+	Effect.incMax = Effect.incMin * Effect.IncMinMaxDiff
+end
+
+function Effect.animate(index)
+    --  Watch for overflow
+    if Effect.ledInc[index] > 0 then
+        if Effect.ledCur[index] >= Effect.ledMax[index] - Effect.ledInc[index] then
+            Effect.ledInc[index] = -Effect.ledInc[index]
+            Effect.ledCur[index] = Effect.ledMax[index]
+            return 1
+        end
+    elseif Effect.ledCur[index] <= Effect.ledInc[index] then
+		Effect.ledInc[index] = -Effect.ledInc[index]
+		Effect.ledCur[index] = 0
+		return -1
+	end
+
+    Effect.ledCur[index] = Effect.ledCur[index] + Effect.ledInc[index]
+    return 0
+end
+
+function Effect.loop()
+	local loopStartTime = millis();
+	for i = 1, NumPixels, 1 do
+		if Effect.animate(i) == -1 then
+			-- We are done with the throb. We always start at 0
+			Effect.ledCur[i] = 0
+			
+			-- Select a new inc (how fast it pulses), and  max (how bright it
+			-- gets) by setting the inc to a random value between incMin and incMax
+			Effect.ledInc[i] = math.random() * (Effect.incMax - Effect.incMin) + Effect.incMin
+			
+			-- set the max brightness to a random value
+			Effect.ledMax[i] = math.random()
+		end
+		
+		-- ledCur is a value between 0 (FlickerBrightestMin) and 1 (brightnessMax)
+		local brightness = Effect.ledCur[i] * (Effect.brightnessMax - Effect.FlickerBrightnessMin) + Effect.FlickerBrightnessMin
+		setLED(1, i - 1, hsvToRGB(Effect.hue, Effect.sat, brightness))
+	end
+	refreshLEDs(1)
+	
+	local delayTime = Effect.Delay - (millis() - loopStartTime)
+	if delayTime < 1 then
+		delayTime = 1
+	end
+
+	return delayTime
+end
+
+return Effect
--- uipanel.lua
+++ uipanel.lua
@@ -1,6 +1,75 @@
 -- Turn lights on and off and set color
 
-local widgetName = arg[1]
-local value = arg[2]
+__print__("Lua UI Panel program running\n")
 
+PixelsPerPost = 8
+NumPosts = 7
+NumPixels = PixelsPerPost * NumPosts
 
+setLEDs(1, 0, NumPixels, 0, 0, 0)
+refreshLEDs(1)
+
+local on = false
+local effect = nil
+local effectName = ""
+
+function showMemoryUsage(s)
+	local before = collectgarbage("count")
+	collectgarbage()
+	local after = collectgarbage("count")
+	__print__(string.format("%s: Mem used before gc=%f, after=%f\n", s, before, after))
+end
+
+function terminateEffect()
+	showMemoryUsage("Before terminateEffect")
+	setLEDs(1, 0, NumPixels, 0, 0, 0)
+	refreshLEDs(1)
+	package.loaded[effectName] = nil
+	effect = nil
+	showMemoryUsage("After terminateEffect")
+end
+
+-- Wait for events
+while true do
+	local ev = getEvent()
+	if ev then
+		for key, value in pairs(ev) do
+			if key == "effect" then
+				if effect then
+					-- terminate the old effect
+					terminateEffect()
+				end
+				
+				effectName = value
+			elseif key == "on" then
+				-- Turn the effect on or off
+				on = value == "true"
+			elseif key == "color" then
+				-- set color
+				__print__(string.format("**** uipanel.lua:color='%s'\n", value))
+			else
+				__print__(string.format("**** uipanel.lua:unknown event, kay='%s', value='%s'\n", key, value))
+			end
+		end
+	end
+	
+	-- run the effect if it's on
+	if effectName ~= "" then
+		if on then
+			if not effect then
+				__print__(string.format("**** running effect '%s'\n", effectName))
+				effect = require(effectName)
+				effect.setup({ 30, 255, 128, 2 }) -- Dummy setup
+			end
+			delay(effect.loop())
+		else
+			-- Effect has been turned off
+			if effect then
+				__print__("**** effect is off, terminating\n")
+				terminateEffect()
+			end
+		end
+	else
+		delay(10)
+	end
+end
